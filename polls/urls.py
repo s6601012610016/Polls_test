c3706beaee568ff9c425692a797f9c535d293440
s6601012610016 <s6601012610016@email.kmutnb.ac.th>
2025-03-13 14:39:58 +0700

    Create private.html

--- polls/urls.py
+++ polls/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "polls"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path("private/", views.PrivateView.as_view(), name="private"),
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
