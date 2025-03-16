cbd4de2c18f53a69d4e7932ffe7c2e614312edb8
s6601012610016 <s6601012610016@email.kmutnb.ac.th>
2025-03-17 02:34:38 +0700

    Completed TEST

--- functional_test.py
+++ functional_test.py
@@ -32,6 +32,26 @@ class TestPrivatePolls(unittest.TestCase):
         self.assertIn("private", self.browser.current_url)
         time.sleep(2)  # เว้นเวลา 2 วินาที
 
+    def test_special_order(self):
+        pass
+        #self.browser.get("http://127.0.0.1:8000/private/")
+        #t.sleep(2)
+        '''
+        Name of username is KRID
+        KRID want to see question about doctor
+        he's decide to go to http://127.0.0.1:8000/ using chrome browser
+        and then UI ask him about what're you interested with?
+        Krid saw that there are many options such as doctor, shopping, game
+        and then Krid clicked on doctor 
+        After than the website put Krid to private question that is about doctor only
+        '''
+        '''
+        Another day Krid want to do something about shopping now
+        so he decide to go to website again and now he choose about shopping
+        and then website move Krid to private question again
+        but now it's all about shopping as he want to
+        '''
+
 if __name__ == "__main__":
     unittest.main()
     
